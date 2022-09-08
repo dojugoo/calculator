48bb66f7d88bedf409b584515638b253a094322d
ulandlau <ulandlau@gmail.com>
2022-09-07 22:19:19 -0700

    Updated look of calculator and working display and button presses updating on screen

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+
+const lineOne = document.querySelector("#line-one");
+const lineTwo = document.querySelector("#line-two");
+
+const zero = document.querySelector("#zero");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+
+zero.addEventListener("click", () => (lineOne.textContent === "") ? "" : lineOne.textContent += 0);
+one.addEventListener("click", () => lineOne.textContent += 1);
+two.addEventListener("click", () => lineOne.textContent += 2);
+three.addEventListener("click", () => lineOne.textContent += 3);
+four.addEventListener("click", () => lineOne.textContent += 4);
+five.addEventListener("click", () => lineOne.textContent += 5);
+six.addEventListener("click", () => lineOne.textContent += 6);
+seven.addEventListener("click", () => lineOne.textContent += 7);
+eight.addEventListener("click", () => lineOne.textContent += 8);
+nine.addEventListener("click", () => lineOne.textContent += 9);
+
 //Computes given operator and two numbers
 function operate(operator, a, b) {
     if (operator === "+") {
